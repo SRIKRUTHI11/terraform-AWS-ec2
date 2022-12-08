4fcbbea60797eb17108172ffc1270e85b4cc964b
Srikruthi Maroju <srikruthi.maroju@cyberark.com>
2022-12-08 16:37:56 +0530

    Terraform Module

---
+++ output.tf
@@ -0,0 +1,7 @@
+output "PublicIP" {
+      value="${aws_instance.myfirst.public_ip}"
+}
+output "PrivateIP" {
+      value="${aws_instance.myfirst.private_ip}"
+}
+
---
+++ variables.tf
@@ -0,0 +1,21 @@
+variable "region" {
+  default = "us-east-1"
+}
+variable "accesskey" {
+  default = ""
+}
+variable "secretkey" {
+  default = ""
+}
+variable "vpcID" {
+  default = "vpc-038b675bf930caa3c"
+}
+variable "amiID" {
+  default = "ami-08e637cea2f053dfa"
+}
+variable "keyPair" {
+  default = "jenkins"
+}
+variable "keyPair1" {
+  default = "jenkins.pem"
+}
